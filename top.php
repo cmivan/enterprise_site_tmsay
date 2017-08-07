025ea031564bfc72fd43b651f0c04c9ab8295dd2
Try <cm.ivan@qq.com>
2017-08-07 10:42:33 +0800

    初始化版本

---
+++ top.php
@@ -0,0 +1,18 @@
+<div class="header">
+    <div id="logo"><a href="index.php">白水一墨的标识</a></div>
+<div class="clearfloat"></div>
+    <ul class="nav">
+      <li style="border:0;"><a href="about.php">品牌简介</a></li>
+      <li><a href="say.php">听木说论</a></li>
+      <li><a href="products.php">产品目录</a></li>
+      <li><a href="feature.php">幕后花絮</a></li>
+      <li><a href="buy.php">购买方式</a></li>
+    </ul>
+<div id="share">
+    <a href="#" id="qq">分享到QQ</a>
+    <a href="#" id="weibo">分享到新浪微博</a>
+    <a href="#" id="weixin">分享到微信</a>
+    <a href="#" id="taobao">分享到淘宝</a>
+    </div>
+    
+    <!-- end .header --></div>
